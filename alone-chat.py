6222aa6ccb6624f3c98447302f71e48b00c7ba76
Tom <qbdxrp+github@pm.me>
2018-12-12 16:23:11 +0100

    Add comments

--- alone-chat.py
+++ alone-chat.py
@@ -21,28 +21,28 @@ def print_welcome():
 
     You can express yourself without hurting anyone
     """
-
     print(welcome)
 
 
 def ask_username():
-    """Ask user username"""
+    """Ask user's username."""
     username = input("Please enter your username: ")
     return username
 
 
 def interface(username):
+    """Display prompt and manage imputs."""
     formatted_username = "[\033[94m" + username + "\033[0m]"
     command = ""
     while command != "/quit" :
         i = datetime.now()
         date_string = i.strftime('%H:%M:%S ')
         command = input(date_string + formatted_username + " > ")
-
     print(username + " \033[91mhas quit")
 
 
 def main():
+    """The beginning of everything"""
     print_welcome()
     username = ask_username()
     interface(username)
