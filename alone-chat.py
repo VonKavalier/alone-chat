ccc6bbecdb3332515c834ca629acbce821d269f9
Tom <von@disroot.org>
2019-01-22 14:18:55 +0100

    Add /hide feature

--- alone-chat.py
+++ alone-chat.py
@@ -73,12 +73,17 @@ def read_command(command):
     """Return value depending on the command."""
     if command.startswith("/"):
         if command == "/help" or command == "/?":
+            print(message_as("system") + "/hide   : Toggle hiding or showing following messages")
             print(message_as("system") + "/export : Export messages from current conversation and quit")
-            print(message_as("system") + "/help   : Display this help")
             print(message_as("system") + "/quit   : Quit Alone-Chat")
+            print(message_as("system") + "/help   : Display this help")
             return 0
         elif command == "/export":
             return 1
+        elif command == "/hide":
+            return 2
+        else:
+            return 3
     else:
         return 0
 
@@ -87,13 +92,22 @@ def interface(username):
     """Display prompt and manage imputs."""
     system_message(username, True)
     formatted_username = message_as(username)
-    message = ""
+    hide = False
+    check_command = 0
     logs = []
-    while read_command(message) == 0:
+    while check_command not in [1,3]:
         message = input(formatted_username)
         logs.append(message)
+        check_command = read_command(message)
+        if check_command == 2:
+            hide = not hide    
+        if hide:
+            CURSOR_UP_ONE = '\x1b[1A'
+            ERASE_LINE = '\x1b[2K'
+            print(CURSOR_UP_ONE + ERASE_LINE)
     if read_command(message) == 1:
         save_logs(logs)
+    
     system_message(username, False)
 
 
